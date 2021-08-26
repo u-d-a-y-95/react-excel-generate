5dc52a27d6cc111c0f9092a4443fa0c5c4a883e8
udaydevbd <uday.dev.bd@gmail.com>
2021-08-26 18:26:01 +0600

    init generate function

--- src/App.js
+++ src/App.js
@@ -4,111 +4,43 @@ import { Workbook } from 'exceljs';
 import * as fs from 'file-saver';
 
 export default function App() {
-  const createRows = () => {
-    return map;
+  const getIndex = char => {
+    return char?.charCodeAt() - 64;
   };
-  const createFile = () => {
-    let workbook = new Workbook();
-    let worksheet = workbook.addWorksheet('Car Data');
-    let companyName = worksheet.addRow(['Akij Poly Fibre Industries Ltd.']);
-    companyName.font = { size: 20, underline: 'single', bold: true };
-    worksheet.mergeCells('A1:L1');
-    worksheet.getCell('A1').alignment = { horizontal: 'center' };
-    let companyLocation = worksheet.addRow([
-      'Akij House, 198 Bir Uttam, Gulshan Link Road, Tejgaon, Dhaka-1208.'
-    ]);
-    companyLocation.font = { size: 14, underline: 'single', bold: true };
-    worksheet.mergeCells('A2:L2');
-    worksheet.getCell('A2').alignment = { horizontal: 'center' };
-    let applicationHeader = worksheet.addRow([
-      `To                                                                                                                                                                  Date: 23 August 2021\nThe Manager\nIslami Bank Bangladesh Ltd.\nHead Office Complex Branch, 40 Dilkusha, C/A, Dhaka-1000\nSubject : Payment Instruction by BEFTN.\n\nDear Sir,\nWe do hereby requesting you to make payment by transferring the amount to the respective Account Holder as shown below in detailed by debiting our CD Account No. \nIBBL: 20502130100096513\nDetailed particulars of each Account Holder:`
-    ]);
+  const createWorkbook = ({ creator = '', createTime = new Date() }) => {
+    const workbook = new Workbook();
+    workbook.creator = creator;
+    workbook.created = createTime;
+    return workbook;
+  };
+  const createFile = excel => {
+    //create workbook
+    const workbook = createWorkbook(excel);
 
-    worksheet.mergeCells('A3:L14');
-    worksheet.getCell('A3').alignment = { horizontal: 'left', wrapText: true };
-    worksheet.addRow([]);
-    let tableHeader = worksheet.addRow([
-      'Account Name',
-      'Code NO',
-      'Bank Name',
-      'Branch Name',
-      'A/C Type',
-      'Account No',
-      'Amount',
-      'Payment Info',
-      'Comments',
-      'Routing No',
-      'Instrument No',
-      'Sl No'
-    ]);
-    tableHeader.font = { bold: true };
-    tableHeader.eachCell(cell => {
-      cell.alignment = { horizontal: 'center' };
+    //generate sheets
+    excel?.sheets?.forEach(sheet => {
+      //create worksheet
+      const _sheet = workbook.addWorksheet(sheet?.name ?? '');
+
+      // generate rows
+      sheet?.rows?.forEach(row => {
+        // preprocess row
+        const _row = [];
+        row?.forEach((cell, index) => {
+          _row[cell?.cellRange ? getIndex(cell?.cellRange[0]) : index] =
+            cell?.text;
+        });
+        wor;
+      });
     });
-    worksheet.addRows([
-      [
-        'SALEHA METAL INDUSTRIES',
-        6587,
-        'BANK ASIA LTD',
-        'JESSORE',
-        'saving',
-        '0003333002081',
-        332000,
-        'SI-APFIL-AUG21-68',
-        'BP-APFIL-AUG21-8',
-        '070410941',
-        'APFIL-118112',
-        1
-      ],
-      [
-        'SALEHA METAL INDUSTRIES',
-        6587,
-        'BANK ASIA LTD',
-        'JESSORE',
-        'saving',
-        '0003333002081',
-        332000,
-        'SI-APFIL-AUG21-68',
-        'BP-APFIL-AUG21-8',
-        '070410941',
-        'APFIL-118112',
-        2
-      ]
-    ]);
-    worksheet.addRow();
-    let bottom1 = worksheet.addRow([
-      'In Word: Fourteen Lakh Twelve Thousand Three Hundred And Twenty Taka Only'
-    ]);
-    bottom1.font = { size: 11, underline: 'single', bold: true };
-    worksheet.mergeCells(`A${bottom1.number}:L${bottom1.number}`);
-    let bottom2 = worksheet.addRow([
-      'For Akij Poly Fibre Industries Ltd. Industries Ltd.'
-    ]);
-    bottom2.font = { size: 11, underline: 'single', bold: true };
-    worksheet.mergeCells(`A${bottom2.number}:L${bottom2.number}`);
-    worksheet.addRow();
-    worksheet.addRow();
-    let bottom3 = worksheet.addRow([
-      'Authorize Signature',
-      '',
-      '',
-      '',
-      'Authorize Signature'
-    ]);
-    worksheet.mergeCells(
-      `A${bottom3.number}:C${bottom3.number},D${bottom3.number}:G${
-        bottom3.number
-      }`
-    );
-    // worksheet.mergeCells(`D${bottom3.number}:F${bottom3.number}`);
+
     workbook.xlsx.writeBuffer().then(data => {
       let blob = new Blob([data], {
         type:
           'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
       });
-      fs.saveAs(blob, 'CarData.xlsx');
+      fs.saveAs(blob, `${excel.name}.xlsx`);
     });
-    console.log('test');
   };
   return (
     <div>
@@ -116,7 +48,35 @@ export default function App() {
       <p>Start editing to see some magic happen :)</p>
       <button
         onClick={() => {
-          createFile();
+          createFile({
+            name: 'TestReport',
+            creator: 'Uday',
+            createTime: new Date(),
+            sheets: [
+              {
+                name: '',
+                rows: [
+                  [
+                    // {
+                    //   text:"",
+                    //   bold:boolean,
+                    //   itatlic:boolean,
+                    //   underline:boolean,
+                    //   font:string,
+                    //   textFomat:enums
+                    //   fontSize:number
+                    //   textColor:"string"
+                    //   bgColor:"string,
+                    //   border:enums,
+                    //   alignment:enums
+                    //   cellRange:String // "A1:A3"
+                    //   merge:boolean
+                    // }
+                  ]
+                ]
+              }
+            ]
+          });
         }}
       >
         Export
